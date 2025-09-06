94edaa65fd88e91042b47dfe05dbabbd3e19cd44
kha <khantps39774@gmail.com>
2025-09-06 16:37:58 +0700

    chinh sua laij ten

--- models/Analystics/dim_city.sql
+++ models/Analystics/dim_city.sql
@@ -2,7 +2,9 @@ WITH dim_city__source AS(
 SELECT  
   *
 FROM vit-lam-data.wide_world_importers.application__cities
-), dim_city__renamed AS(
+), 
+
+dim_city__renamed AS(
 SELECT
   city_id AS city_key,
   city_name,
@@ -14,9 +16,22 @@ SELECT
   CAST(city_name AS STRING) AS city_name,
   CAST(state_province_key AS INTEGER) AS state_province_key
 FROM dim_city__renamed
+), 
+
+dim_city__join AS (
+SELECT
+  city.city_key,
+  city.city_name,
+  city.state_province_key,
+  state.delivier_state_province_name
+FROM dim_city__convert as city
+LEFT JOIN {{ref('stg_dim_delivier_province_state')}} AS state
+ON city.state_province_key = state.delivier_state_province_key
 )
+
 SELECT
   city_key,
   city_name,
-  state_province_key
-FROM dim_city__convert
+  state_province_key,
+  delivier_state_province_name
+FROM dim_city__join AS city
---
+++ target/compiled/wide_world_importers_dbt/models/Staging/stg_dim_province_state.sql
@@ -0,0 +1,18 @@
+with dim_province_state__source AS(
+SELECT *
+FROM vit-lam-data.wide_world_importers.application__state_provinces
+), dim_province_state__renamed AS(
+SELECT
+  state_province_id AS state_province_key,
+  state_province_name AS state_province_name
+FROM dim_province_state__source
+), dim_province_state__convert AS(
+SELECT
+  CAST(state_province_key AS INTEGER) AS state_province_key,
+  CAST(state_province_name AS STRING) AS state_province_name
+FROM dim_province_state__renamed
+)
+SELECT
+  state_province_key,
+  state_province_name
+FROM dim_province_state__convert
