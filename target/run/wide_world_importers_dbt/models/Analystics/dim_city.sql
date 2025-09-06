a14f27f20697ddf757e0252e0182ecb4b74b8e52
kha <khantps39774@gmail.com>
2025-09-06 16:23:44 +0700

    tao dim_city voi stg dim_province

---
+++ target/run/wide_world_importers_dbt/models/Analystics/dim_city.sql
@@ -0,0 +1,27 @@
+
+
+  create or replace view `project-github-471211`.`wide_world_importers_dwh`.`dim_city`
+  OPTIONS()
+  as WITH dim_city__source AS( 
+SELECT  
+  *
+FROM vit-lam-data.wide_world_importers.application__cities
+), dim_city__renamed AS(
+SELECT
+  city_id AS city_key,
+  city_name,
+  state_province_id AS state_province_key
+FROM dim_city__source
+), dim_city__convert AS(
+SELECT
+  CAST(city_key AS INTEGER) AS city_key,
+  CAST(city_name AS STRING) AS city_name,
+  CAST(state_province_key AS INTEGER) AS state_province_key
+FROM dim_city__renamed
+)
+SELECT
+  city_key,
+  city_name,
+  state_province_key
+FROM dim_city__convert;
+
