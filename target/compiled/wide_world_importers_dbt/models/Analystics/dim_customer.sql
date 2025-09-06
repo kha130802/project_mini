1262ba29b96a770f5e1afb65867d053bbb2bffcb
kha <khantps39774@gmail.com>
2025-09-06 12:48:55 +0700

    them file dim_customer

---
+++ target/compiled/wide_world_importers_dbt/models/Analystics/dim_customer.sql
@@ -0,0 +1,8 @@
+WITH dim_customer AS(
+  SELECT
+    *
+  FROM vit-lam-data.wide_world_importers.sales__customers
+)
+SELECT
+  *
+FROM dim_customer
---
+++ target/run/wide_world_importers_dbt/models/Analystics/dim_customer.sql
@@ -0,0 +1,13 @@
+
+
+  create or replace view `project-github-471211`.`wide_world_importers_dwh`.`dim_customer`
+  OPTIONS()
+  as WITH dim_customer AS(
+  SELECT
+    *
+  FROM vit-lam-data.wide_world_importers.sales__customers
+)
+SELECT
+  *
+FROM dim_customer;
+
