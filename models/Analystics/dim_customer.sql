446ea00b752ea5e55fe9b7d2a41ad4533b75f101
kha <khantps39774@gmail.com>
2025-09-07 17:03:17 +0700

    union all bang dim customer va case when type boolean

--- models/Analystics/dim_customer.sql
+++ models/Analystics/dim_customer.sql
@@ -1,69 +1,142 @@
-WITH dim_customer__source AS(
-  SELECT
-    *
-  FROM vit-lam-data.wide_world_importers.sales__customers
-), dim_customer__renamed AS(
-  SELECT
-    customer_id AS customer_key,
-    customer_name,
-    is_statement_sent,
-    is_on_credit_hold,
-    payment_days,
-    credit_limit,
-    account_opened_date,
-    customer_category_id as customer_category_key,
-    buying_group_id as customer_buying_group_key,
-    delivery_method_id as delivery_method_key,
-    delivery_city_id as delivery_city_key,
-    postal_city_id as postal_city_key,
-    primary_contact_person_id as primary_contact_person_key,
-    bill_to_customer_id as bill_to_customer_key
-  FROM dim_customer__source
-), dim_customer__convert as(
-SELECT
-  CAST(customer_key AS INTEGER) AS customer_key,
-  CAST(customer_name AS STRING) AS customer_name,
-  CAST(is_statement_sent AS BOOL) AS is_statement_sent,
-  CAST(is_on_credit_hold AS BOOL) AS is_on_credit_hold,
-  CAST(payment_days AS INTEGER) AS payment_days,
-  CAST(credit_limit AS FLOAT64) AS credit_limit,
-  CAST(account_opened_date AS DATE) AS account_opened_date,
-  CAST(customer_category_key AS INTEGER) AS customer_category_key,
-  CAST(customer_buying_group_key AS INTEGER) AS customer_buying_group_key,
-  CAST(delivery_method_key AS INTEGER) AS delivery_method_key,
-  CAST(delivery_city_key AS INTEGER) AS delivery_city_key,
-  CAST(postal_city_key AS INTEGER) AS postal_city_key,
-  CAST(primary_contact_person_key AS INTEGER) AS primary_contact_person_key,
-  CAST(bill_to_customer_key AS INTEGER) AS bill_to_customer_key
-FROM dim_customer__renamed
-), dim_customer__join AS (
-SELECT
-  customer.customer_key,
-  customer.customer_name,
-  customer.is_statement_sent,
-  customer.is_on_credit_hold,
-  customer.payment_days,
-  customer.credit_limit,
-  customer.account_opened_date,
-  customer.customer_category_key,
-  category.customer_category_name,
-  customer.customer_buying_group_key,
-  buying_group.buying_group_name,
-  customer.delivery_method_key,
-  customer.delivery_city_key,
-  city.city_name AS delivery_city_name,
-  city.state_province_key AS delivery_state_province_key,
-  city.state_province_name AS delivery_state_province_name,
-  customer.postal_city_key,
-  city.city_name AS postal_city_name,
-  city.state_province_key AS postal_state_province_key,
-  city.state_province_name AS postal_state_province_name,
-  customer.primary_contact_person_key,
-  person.full_name AS primary_contact_full_name,
-  person.preferred_name AS primary_contact_preferred_name,
-  person.search_name AS primary_contact_search_name,
-  customer.bill_to_customer_key
-FROM dim_customer__convert AS customer
+WITH dim_customer__source AS(  
+  SELECT *  
+  FROM `vit-lam-data.wide_world_importers.sales__customers`  
+),  
+
+dim_customer__renamed AS(  
+  SELECT  
+    customer_id AS customer_key,  
+    customer_name,  
+    is_statement_sent,  
+    is_on_credit_hold,  
+    payment_days,  
+    credit_limit,  
+    account_opened_date,  
+    customer_category_id AS customer_category_key,  
+    buying_group_id AS customer_buying_group_key,  
+    delivery_method_id AS delivery_method_key,  
+    delivery_city_id AS delivery_city_key,  
+    postal_city_id AS postal_city_key,  
+    primary_contact_person_id AS primary_contact_person_key,  
+    bill_to_customer_id AS bill_to_customer_key  
+  FROM dim_customer__source  
+),  
+
+dim_customer__convert AS (  
+  SELECT  
+    CAST(customer_key AS INT64) AS customer_key,  
+    CAST(customer_name AS STRING) AS customer_name,  
+    CAST(is_statement_sent AS BOOL) AS is_statement_sent,  
+    CAST(is_on_credit_hold AS BOOL) AS is_on_credit_hold,  
+    CAST(payment_days AS INT64) AS payment_days,  
+    CAST(credit_limit AS FLOAT64) AS credit_limit,  
+    CAST(account_opened_date AS DATE) AS account_opened_date,  
+    CAST(customer_category_key AS INT64) AS customer_category_key,  
+    CAST(customer_buying_group_key AS INT64) AS customer_buying_group_key,  
+    CAST(delivery_method_key AS INT64) AS delivery_method_key,  
+    CAST(delivery_city_key AS INT64) AS delivery_city_key,  
+    CAST(postal_city_key AS INT64) AS postal_city_key,  
+    CAST(primary_contact_person_key AS INT64) AS primary_contact_person_key,  
+    CAST(bill_to_customer_key AS INT64) AS bill_to_customer_key  
+  FROM dim_customer__renamed  
+),  
+
+dim_customer_convert__boolean AS (  
+  SELECT  
+    *,  
+    CASE  
+      WHEN is_statement_sent IS TRUE THEN 'on_is_statement_sent'  
+      WHEN is_statement_sent IS FALSE THEN 'off_is_statement_sent'  
+      ELSE 'unknown_is_statement_sent'  
+    END AS is_statement_sent_flag,  
+    CASE  
+      WHEN is_on_credit_hold IS TRUE THEN 'on_is_on_credit_hold'  
+      WHEN is_on_credit_hold IS FALSE THEN 'off_is_on_credit_hold'  
+      ELSE 'unknown_is_on_credit_hold'  
+    END AS is_on_credit_hold_flag  
+  FROM dim_customer__convert  
+),  
+
+dim_customer_undefined__record AS (  
+  SELECT   
+    customer_key,  
+    customer_name,  
+    is_statement_sent_flag,
+    is_on_credit_hold_flag,
+    account_opened_date,  
+    payment_days,  
+    credit_limit,  
+    customer_category_key,  
+    customer_buying_group_key,  
+    delivery_method_key,  
+    delivery_city_key,  
+    postal_city_key,  
+    primary_contact_person_key,  
+    bill_to_customer_key  
+  FROM dim_customer_convert__boolean  
+
+  UNION ALL  
+
+  SELECT  
+    0 AS customer_key,  
+    'undefined' AS customer_name,  
+    'unknown_is_statement_sent' AS is_statement_sent_flag,  
+    'unknown_is_on_credit_hold' AS is_on_credit_hold_flag,  
+    DATE '1970-01-01' AS account_opened_date,  
+    0 AS payment_days,  
+    0.0 AS credit_limit,  
+    0 AS customer_category_key,  
+    0 AS customer_buying_group_key,  
+    0 AS delivery_method_key,  
+    0 AS delivery_city_key,  
+    0 AS postal_city_key,  
+    0 AS primary_contact_person_key,  
+    0 AS bill_to_customer_key  
+
+  UNION ALL  
+
+  SELECT  
+    -1 AS customer_key,  
+    'invalid' AS customer_name,  
+    'unknown_is_statement_sent' AS is_statement_sent_flag,  
+    'unknown_is_on_credit_hold' AS is_on_credit_hold_flag,  
+    DATE '1970-01-01' AS account_opened_date,  
+    -1 AS payment_days,  
+    -1.0 AS credit_limit,  
+    -1 AS customer_category_key,  
+    -1 AS customer_buying_group_key,  
+    -1 AS delivery_method_key,  
+    -1 AS delivery_city_key,  
+    -1 AS postal_city_key,  
+    -1 AS primary_contact_person_key,  
+    -1 AS bill_to_customer_key  
+),  
+
+dim_customer__join AS (  
+  SELECT  
+    customer.customer_key,
+    customer.customer_name,
+    customer.is_statement_sent_flag AS is_statement_send,
+    customer.is_on_credit_hold_flag AS is_on_credit_holds,
+    customer.payment_days,
+    customer.credit_limit,
+    customer.account_opened_date,
+    customer.customer_category_key,
+    category.customer_category_name,
+    customer.customer_buying_group_key,
+    buying_group.buying_group_name,
+    customer.delivery_method_key,
+    customer.delivery_city_key,
+    city.city_name AS delivery_city_name,
+    city.state_province_key AS delivery_state_province_key,
+    city.state_province_name AS delivery_state_province_name,
+    customer.postal_city_key,
+    city.city_name AS postal_city_name,
+    customer.primary_contact_person_key,
+    person.full_name ,
+    person.preferred_name ,
+    person.search_name 
+FROM dim_customer_undefined__record AS customer
 LEFT JOIN {{ref('stg_dim_category')}} AS category
 ON customer.customer_category_key = category.customer_category_key
 LEFT JOIN {{ref('stg_dim_buying_group')}} AS buying_group
@@ -72,13 +145,12 @@ LEFT JOIN {{ref('dim_city')}} AS city
 ON customer.delivery_city_key = city.city_key
 LEFT JOIN {{ref('stg_dim_person')}} AS person
 ON customer.primary_contact_person_key = person.person_key
-
 )
 SELECT 
   customer_key,
   customer_name,
-  is_statement_sent,
-  is_on_credit_hold,
+  is_statement_send,
+  is_on_credit_holds,
   payment_days,
   credit_limit,
   account_opened_date,
@@ -88,10 +160,11 @@ SELECT
   buying_group_name,
   delivery_method_key,
   delivery_city_key,
+  delivery_city_name,
   postal_city_key,
+  postal_city_name,
   primary_contact_person_key,
-  primary_contact_full_name,
-  primary_contact_preferred_name,
-  primary_contact_search_name,
-  bill_to_customer_key
+  full_name ,
+  preferred_name ,
+  search_name 
 FROM dim_customer__join
--- target/compiled/wide_world_importers_dbt/models/Staging/stg_dim_buying_group.sql
+++ target/compiled/wide_world_importers_dbt/models/Staging/stg_dim_buying_group.sql
@@ -11,8 +11,23 @@ WITH stg_dim_buying_group__source AS (
     CAST(buying_group_key AS INTEGER) AS buying_group_key,
     CAST(buying_group_name AS STRING) AS buying_group_name
   FROM stg_dim_buying_group__renamed
-)
+), stg_dim_buying_group_undefined AS (
   SELECT
     buying_group_key,
     buying_group_name
-  FROM stg_dim_buying_group__convert
+  FROM stg_dim_buying_group__convert
+
+  UNION ALL
+  SELECT
+    0 AS buying_group_key,
+    'Undefined' AS buying_group_name
+
+  UNION ALL
+  SELECT
+    -1 AS buying_group_key,
+    'Invalid' AS buying_group_name
+)
+SELECT
+  buying_group_key,
+  buying_group_name
+FROM stg_dim_buying_group_undefined
